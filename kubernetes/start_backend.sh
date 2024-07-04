ab62d67ac647cddb2a89b469daad002c3688c0f3
Hoan Le <ledaihoan@gmail.com>
2024-07-04 23:35:20 +0700

    feat: add deployment script for backend

--- kubernetes/start_backend.sh
+++ kubernetes/start_backend.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PROJECT_PATH=$CURRENT_DIR/../../s8-fd-server
 
 if [ -n "$1" ]; then
   DEPLOYMENT_ENV=$1
@@ -8,11 +7,4 @@ else
   DEPLOYMENT_ENV=development
 fi
 
-cd $PROJECT_PATH
-./update_docker_build.sh $3
-cd $CURRENT_DIR
-if [ -n "$3" ]; then
-  kubectl apply -f $CURRENT_DIR/$DEPLOYMENT_ENV/s8-fd-server.deployment.yaml
-else
-  kubectl apply -f $CURRENT_DIR/$DEPLOYMENT_ENV/s8-fd-server.deployment.yaml
-fi
+kubectl apply -f $CURRENT_DIR/$DEPLOYMENT_ENV/s8-fd-server.deployment.yaml
---
+++ kubernetes/stop_backend.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+kubectl delete deployment s8-fd-server -n s8fd
